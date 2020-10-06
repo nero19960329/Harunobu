7ad4b453bbf9e051f3f529dc1d896fc7ce718017
nero19960329 <wangzhao2013tsinghua@gmail.com>
2020-10-06 22:37:53 +0800

    add clang-format

--- src/core/camera.cpp
+++ src/core/camera.cpp
@@ -2,21 +2,17 @@
 
 namespace harunobu {
 
-Camera::Camera() :
-    pos(0.0, 0.0, 0.0),
-    dir(0.0, 0.0, 1.0),
-    up(0.0, 1.0, 0.0),
-    fov(30),
-    height(1920),
-    width(1080) {}
+Camera::Camera()
+    : pos(0.0, 0.0, 0.0), dir(0.0, 0.0, 1.0), up(0.0, 1.0, 0.0), fov(30),
+      height(1920), width(1080) {}
 
 void Camera::log_current_status() const {
-    HARUNOBU_INFO("Camera");
-    HARUNOBU_INFO("--- pos = {}", glm::to_string(pos));
-    HARUNOBU_INFO("--- dir = {}", glm::to_string(dir));
-    HARUNOBU_INFO("--- up = {}", glm::to_string(up));
-    HARUNOBU_INFO("--- fov = {}", fov);
-    HARUNOBU_INFO("--- h, w = {}, {}", height, width);
+  HARUNOBU_INFO("Camera");
+  HARUNOBU_INFO("--- pos = {}", glm::to_string(pos));
+  HARUNOBU_INFO("--- dir = {}", glm::to_string(dir));
+  HARUNOBU_INFO("--- up = {}", glm::to_string(up));
+  HARUNOBU_INFO("--- fov = {}", fov);
+  HARUNOBU_INFO("--- h, w = {}, {}", height, width);
 }
 
 } // namespace harunobu
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,17 @@
-#include <string>
 #include <iostream>
+#include <string>
 
 #include <harunobu/core/common.h>
 #include <harunobu/io/mitsuba_reader.h>
 
-int main(int argc, char* argv[]) {
-    harunobu::initialize_env();
+int main(int argc, char *argv[]) {
+  harunobu::initialize_env();
 
-    HARUNOBU_CHECK(argc == 2, "Invalid argument! Usage: harunobu <scene_xml path>");
+  HARUNOBU_CHECK(argc == 2,
+                 "Invalid argument! Usage: harunobu <scene_xml path>");
 
-    harunobu::MitsubaReader scene_reader;
-    scene_reader.load(std::string(argv[1]));
+  harunobu::MitsubaReader scene_reader;
+  scene_reader.load(std::string(argv[1]));
 
-    return 0;
+  return 0;
 }
